a4abb405ca79f065249f28ff618a078c10d4eb33
Jacob Duijzer <jacob@duijzer.com>
2019-10-10 11:11:03 +0200

    Added more unittests to test adults with adhd (same outcome)

--- src/Decision.UnitTests/BarTenderServiceShould.cs
+++ src/Decision.UnitTests/BarTenderServiceShould.cs
@@ -1,10 +1,7 @@
-using System.Diagnostics;
-using Bogus;
 using Decision.Core;
 using FluentAssertions;
 using Xunit;
 using Xunit.Abstractions;
-using Person = Decision.Core.Person;
 
 namespace Decision.UnitTests
 {
@@ -27,45 +24,21 @@ namespace Decision.UnitTests
         [InlineData(Gender.Male, 10, false, "Coke")]
         [InlineData(Gender.Male, 10, true, "Apple juice")]
         [InlineData(Gender.Male, 18, false, "Beer")]
+        [InlineData(Gender.Male, 18, true, "Beer")]
         [InlineData(Gender.Male, 40, false, "Beer")]
+        [InlineData(Gender.Male, 40, true, "Beer")]
         [InlineData(Gender.Male, 60, false, "Whiskey")]
+        [InlineData(Gender.Male, 60, true, "Whiskey")]
         [InlineData(Gender.Female, 10, false, "Fanta")]
         [InlineData(Gender.Female, 10, true, "Orange juice")]
         [InlineData(Gender.Female, 18, false, "Wine")]
+        [InlineData(Gender.Female, 18, true, "Wine")]
         [InlineData(Gender.Female, 31, false, "Beer")]
+        [InlineData(Gender.Female, 31, true, "Beer")]
         [InlineData(Gender.Female, 55, false, "Wine")]
+        [InlineData(Gender.Female, 55, true, "Wine")]
         public void ServeProperDrink(Gender gender, int age, bool hasAdhd, string expectedDrink) =>
             _service.ServeDrink(new Person { Age = age, Gender = gender, HasAdhd = hasAdhd })
                 .Should().Be(expectedDrink);
-
-        [Fact]
-        public void Perform()
-        {
-            var people = new Faker<Person>()
-                .RuleFor(x => x.Age, (faker, person) => faker.Random.Int(4, 90))
-                .RuleFor(x => x.FirstName, (faker, person) => faker.Person.FirstName)
-                .RuleFor(x => x.LastName, (faker, person) => faker.Person.LastName)
-                .RuleFor(x => x.Gender, (faker, person) => faker.PickRandom(Gender.Female, Gender.Male))
-                .RuleFor(x => x.HasAdhd, (faker, person) => faker.PickRandom(true, false))
-                .Generate(1000000);
-
-            Stopwatch sw = new Stopwatch();
-            //Dictionary<string, int> record = new Dictionary<string, int>();
-            sw.Start();
-            people.ForEach(person =>
-            {
-                var result = _service.ServeDrink(person);
-//                if (record.ContainsKey(result))
-//                    record[result]++;
-//                else
-//                    record[result] = 1;
-            });
-
-            sw.Stop();
-            _output.WriteLine($"Elapsed={sw.Elapsed}");
-
-//            foreach (var keyValuePair in record)
-//                _output.WriteLine($"{keyValuePair.Key}: {keyValuePair.Value}");
-        }
     }
 }
